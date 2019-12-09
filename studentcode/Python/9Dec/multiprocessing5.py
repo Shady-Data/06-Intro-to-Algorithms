5e44ff7e83c7f666f48aab318af1819b985ce5af
Robert Chunn <rechunn@gmail.com>
2019-12-09 13:24:02 -0600

    Added multiprocessing 4 and 5 work

--- studentcode/Python/9Dec/multiprocessing5.py
+++ studentcode/Python/9Dec/multiprocessing5.py
@@ -4,6 +4,9 @@ Prove that these are actually coming in as completed,
 
 Start 5 second thread first, but since we used as_completed() method it prints the results
     in the order they are completed
+
+With the submit() method, it submits each function one at a time,
+    so we can use the submit() method on an entire list by using map
 '''
 
 import concurrent.futures
@@ -19,20 +22,25 @@ if __name__ == '__main__':
     start = time.perf_counter()
 
     # using a context manager
-    with concurrent.futures.ProcessPoolExecutor() as executor:
+    with concurrent.futures.ThreadPoolExecutor() as executor:
         # submit schedules a function to be executed one at a time and returns a future object
         # f1 = executor.submit(do_something, 1)
         # f2 = executor.submit(do_something, 1)
         
         # print(f1.result())
         # print(f2.result())
-        # seconds_list = [5, 4, 3, 2, 1]
+        seconds_list = [5, 4, 3, 2, 1]
         # results = [executor.submit(do_something, sec) for sec in seconds_list]
-        results = [executor.submit(do_something, sec) for sec in range(5, 0, -1)]
+        # results = [executor.submit(do_something, sec) for sec in range(5, 0, -1)]
+
+        # map will apply the function do_something() to every item in the seconds_list
+        # instead of running the results as complete, map returns in the order that they were started
+
+        results = executor.map(do_something, seconds_list)
 
         # to get the results we can use another funtion, as_completed(), from future object that gives us an iterator
-        for f in concurrent.futures.as_completed(results):
-            print(f.result())
+        for result in results:
+            print(result)
 
     finish = time.perf_counter()
 
--- studentcode/Python/9Dec/multithreading4.py
+++ studentcode/Python/9Dec/multithreading4.py
@@ -4,9 +4,6 @@ Prove that these are actually coming in as completed,
 
 Start 5 second thread first, but since we used as_completed() method it prints the results
     in the order they are completed
-
-With the submit() method, it submits each function one at a time,
-    so we can use the submit() method on an entire list by using map
 '''
 
 import concurrent.futures
@@ -30,9 +27,6 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
     # seconds_list = [5, 4, 3, 2, 1]
     # results = [executor.submit(do_something, sec) for sec in seconds_list]
     results = [executor.submit(do_something, sec) for sec in range(5, 0, -1)]
-
-    # map will apply the function do_something() to every item in the seconds_list
-    # instead of running the results as complete, map returns in the order that they were started
 
     # to get the results we can use another funtion, as_completed(), from future object that gives us an iterator
     for f in concurrent.futures.as_completed(results):
